4eef2344ec4519a5cf9f373b20986cc3fe509734
Nic Fontaine <ngpfontaine@gmail.com>
2017-09-15 10:01:25 -0700

    moved evoluent settings into xstartup

--- bashrc
+++ bashrc
@@ -125,3 +125,5 @@ function _update_ps1() {
 if [ "$TERM" != "linux" ]; then
     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
 fi
+
+PATH=$PATH:/opt/jaxx/Jaxx
--- xinput-evoluent.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xinput set-button-map "Kingsis Peripherals Evoluent VerticalMouse 4" 1 3 0 4 5 6 7 8 2 2
-#xinput set-button-map "Kingsis Peripherals Evoluent VerticalMouse 4" 1 3 2 4 5 6 7 8 2 2
-#btn1, btn2, btn3, scrollup, scrolldown, nothing, nothing, thumbtop, middleclick, thumbdown
